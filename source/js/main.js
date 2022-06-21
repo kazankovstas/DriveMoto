7cd17784f4de915d0fd74a55abbbafe30cbff3cb
Stanislav <kazankovstas.jb@gmail.com>
2022-06-21 12:58:55 +0300

    feat: adaptive header&banner

--- source/js/main.js
+++ source/js/main.js
@@ -2,8 +2,32 @@ $(function () {
   $(".banner__slider-list").slick({
     dots: true,
     prevArrow:
-      '<button class="slick-btn banner-btn banner-btn__left"><img src="img/sprite/big-arrow-left.svg" alt=""></button>',
+      '<button class="slick-btn banner-btn banner-btn__left"><svg width="16" height="29"><use xlink:href="img/sprite.svg#big-arrow-left"></use></svg></button>',
     nextArrow:
-      '<button class="slick-btn banner-btn banner-btn__right"><img src="img/sprite/big-arrow-right.svg" alt=""></button>',
+      '<button class="slick-btn banner-btn banner-btn__right"><svg width="16" height="29"><use xlink:href="img/sprite.svg#big-arrow-right"></use></svg></button>',
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
   });
 });
+
+{
+  const openMenu = document.querySelector(".main-nav__toggle");
+  const menu = document.querySelector(".menu-mobile");
+
+  openMenu.addEventListener("click", function () {
+    openMenu.classList.toggle("main-nav__toggle_active");
+    openMenu.classList.contains("main-nav__toggle_active")
+      ? menu.classList.add("menu-mobile_active")
+      : menu.classList.remove("menu-mobile_active");
+  });
+}
+
+{
+  document.querySelector("slick-list").tabIndex = -1;
+}
