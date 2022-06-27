ece1a6f70c3beae04abfce8fe064daea77a5912c
Stanislav <kazankovstas.jb@gmail.com>
2022-06-27 20:23:15 +0300

    feat: catalog-form

--- source/js/main.js
+++ source/js/main.js
@@ -77,6 +77,11 @@ $(function () {
       },
     ],
   });
+
+  // $(".form__drop-down").on("click", function () {
+  //   $(this).toggleClass("form__drop-down_active");
+  //   $(this).next(".form__content").slideToggle();
+  // });
 });
 
 {
@@ -121,6 +126,132 @@ $(function () {
     openMenuStore.classList.contains("store__drop-down_active")
       ? menuStore.classList.add("store__list_active")
       : menuStore.classList.remove("store__list_active");
+  });
+}
+
+// -------------------------------------------------------------------------- Дроп-меню Форма-Наличие
+{
+  const openMenuAvailable = document.querySelector(".available__drop-down");
+  const menuAvailable = document.querySelector(".available__list");
+  const iconAvailable = document.querySelector(".available__icon");
+
+  openMenuAvailable.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    openMenuAvailable.classList.toggle("available__drop-down_hide");
+    iconAvailable.classList.toggle("available__icon_hide");
+    openMenuAvailable.classList.contains("available__drop-down_hide")
+      ? menuAvailable.classList.add("available__list_hide")
+      : menuAvailable.classList.remove("available__list_hide");
+  });
+}
+
+// -------------------------------------------------------------------------- Дроп-меню Форма-Новинки
+{
+  const openMenuNews = document.querySelector(".news__drop-down");
+  const menuNews = document.querySelector(".news__list");
+  const iconNews = document.querySelector(".news__icon");
+
+  openMenuNews.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    openMenuNews.classList.toggle("news__drop-down_hide");
+    iconNews.classList.toggle("news__icon_hide");
+    openMenuNews.classList.contains("news__drop-down_hide")
+      ? menuNews.classList.add("news__list_hide")
+      : menuNews.classList.remove("news__list_hide");
+  });
+}
+
+// -------------------------------------------------------------------------- Дроп-меню Форма-Цена
+{
+  const openMenuPrice = document.querySelector(".price__drop-down");
+  const menuPrice = document.querySelector(".price__list");
+  const iconPrice = document.querySelector(".price__icon");
+
+  openMenuPrice.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    openMenuPrice.classList.toggle("price__drop-down_hide");
+    iconPrice.classList.toggle("price__icon_hide");
+    openMenuPrice.classList.contains("price__drop-down_hide")
+      ? menuPrice.classList.add("price__list_hide")
+      : menuPrice.classList.remove("price__list_hide");
+  });
+}
+
+// -------------------------------------------------------------------------- Дроп-меню Форма-Бренд
+{
+  const openMenuBrand = document.querySelector(".brand__drop-down");
+  const menuBrand = document.querySelector(".brand__list");
+  const iconBrand = document.querySelector(".brand__icon");
+
+  openMenuBrand.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    openMenuBrand.classList.toggle("brand__drop-down_hide");
+    iconBrand.classList.toggle("brand__icon_hide");
+    openMenuBrand.classList.contains("brand__drop-down_hide")
+      ? menuBrand.classList.add("brand__list_hide")
+      : menuBrand.classList.remove("brand__list_hide");
+  });
+}
+
+// -------------------------------------------------------------------------- Дроп-меню Форма-Модели
+{
+  const openMenuModels = document.querySelector(".models__drop-down");
+  const menuModels = document.querySelector(".models__list");
+  const iconModels = document.querySelector(".models__icon");
+
+  openMenuModels.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    openMenuModels.classList.toggle("models__drop-down_hide");
+    iconModels.classList.toggle("models__icon_hide");
+    openMenuModels.classList.contains("models__drop-down_hide")
+      ? menuModels.classList.add("models__list_hide")
+      : menuModels.classList.remove("models__list_hide");
+  });
+}
+
+{
+  const modelsMore = document.querySelector(".models-more");
+  const modelList = document.querySelector(".model__list_hide");
+
+  modelsMore.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modelsMore.innerHTML =
+      modelsMore.innerHTML === "Показать еще"
+        ? (modelsMore.innerHTML = "Скрыть")
+        : (modelsMore.innerHTML = "Показать еще");
+    modelList.classList.toggle("model__list_hide");
+  });
+}
+
+// -------------------------------------------------------------------------- Дроп-меню Форма-Скидки
+{
+  const openMenuSales = document.querySelector(".sales__drop-down");
+  const menuSales = document.querySelector(".sales__list");
+  const iconSales = document.querySelector(".sales__icon");
+
+  openMenuSales.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    openMenuSales.classList.toggle("sales__drop-down_hide");
+    iconSales.classList.toggle("sales__icon_hide");
+    openMenuSales.classList.contains("sales__drop-down_hide")
+      ? menuSales.classList.add("sales__list_hide")
+      : menuSales.classList.remove("sales__list_hide");
+  });
+}
+
+// -------------------------------------------------------------------------- Дроп-меню Форма-Страны
+{
+  const openMenuCountries = document.querySelector(".countries__drop-down");
+  const menuCountries = document.querySelector(".countries__list");
+  const iconCountries = document.querySelector(".countries__icon");
+
+  openMenuCountries.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    openMenuCountries.classList.toggle("countries__drop-down_hide");
+    iconCountries.classList.toggle("countries__icon_hide");
+    openMenuCountries.classList.contains("countries__drop-down_hide")
+      ? menuCountries.classList.add("countries__list_hide")
+      : menuCountries.classList.remove("countries__list_hide");
   });
 }
 
